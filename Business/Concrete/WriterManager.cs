ce820c071670651baf762ac08f392c1e639acfe7
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-07-18 11:25:24 +0300

    changes added

---
+++ Business/Concrete/WriterManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class WriterManager : IWriterService
+    {
+        private readonly IWriterDal _writerDal;
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+        public void AddWriter(Writer writer)
+        {
+            _writerDal.Insert(writer);
+        }
+    }
+}
---
+++ coreProject2/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreProject2.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            writer.WriterStatus = true;
+            writer.WriterAbout = "Deneme yazısı";
+            writerManager.AddWriter(writer);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
