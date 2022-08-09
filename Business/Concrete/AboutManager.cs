be1f0152b8efcbc7208d8f387d130df59dbc4a53
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-08-09 17:44:48 +0300

    about operations added

---
+++ Business/Concrete/AboutManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDal _aboutDal;
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+        public List<About> GetList()
+        {
+            return _aboutDal.GetAll();
+        }
+    }
+}
---
+++ coreProject2/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreProject2.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var value = aboutManager.GetList();
+            return View(value);
+        }
+
+        [HttpGet]
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
+}
