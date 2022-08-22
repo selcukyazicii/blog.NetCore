b63240db206fb784115b66870a837939b3bbd0d7
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-08-22 16:46:13 +0300

    notification services added to architecture

---
+++ Business/Concrete/NotificationManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class NotificationManager : INotificationServices
+    {
+        private readonly INotificationDal _notificationDal;
+        public NotificationManager(INotificationDal notificationDal)
+        {
+            _notificationDal = notificationDal;
+        }
+        public void Add(Notification t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Notification t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Notification GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Notification> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Notification t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
