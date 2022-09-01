0e64f5bcca7f965e42810f5160136f410183cf7e
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-09-01 10:44:16 +0300

    areas folder added

--- coreProject2/Startup.cs
+++ coreProject2/Startup.cs
@@ -77,6 +77,10 @@ namespace coreProject2
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
