0d3acbbee843acedf065e7b7511ea63874023e60
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-09-01 11:07:11 +0300

    paged list added on admin page

--- coreProject2/Areas/Admin/Controllers/CategoryController.cs
+++ coreProject2/Areas/Admin/Controllers/CategoryController.cs
@@ -5,16 +5,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using X.PagedList;
 namespace coreProject2.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
         CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            var result = _categoryManager.GetList();
+            var result = _categoryManager.GetList().ToPagedList(page,3);
             return View(result);
         }
     }
