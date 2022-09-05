7a97f399e2674c5758140a215b190051d7ceaa8b
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-09-05 15:06:15 +0300

    statistic

---
+++ Business/Concrete/AdminManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+   public class AdminManager:IAdminService
+    {
+        private readonly IAdminDal _adminDal;
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+
+        public void Add(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Admin GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Admin> GetList()
+        {
+            return _adminDal.GetAll();
+        }
+
+        public void Update(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- coreProject2/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
+++ coreProject2/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,12 @@ namespace coreProject2.Areas.Admin.ViewComponents.Statistic
 {
     public class Statistic4:ViewComponent
     {
+        Context _context = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.AdminName = _context.Admins.Where(x => x.AdminId == 1).Select(x => x.Name).FirstOrDefault();
+            ViewBag.AdminImage = _context.Admins.Where(x => x.AdminId == 1).Select(x => x.ImageUrl).FirstOrDefault();
+            ViewBag.AdminDescription = _context.Admins.Where(x => x.AdminId == 1).Select(x => x.ShortDescription).FirstOrDefault();
             return View();
         }
     }
