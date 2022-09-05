d1b8b56af7815f79e4e3e6ffadfa5bf225167125
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-09-05 16:38:52 +0300

    weathermap added

--- coreProject2/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ coreProject2/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace coreProject2.Areas.Admin.ViewComponents.Statistic
 {
@@ -19,6 +20,10 @@ namespace coreProject2.Areas.Admin.ViewComponents.Statistic
             ViewBag.totalBlog = _blogManager.GetList().Count();
             ViewBag.totalContact = _context.Contacts.Count();
             ViewBag.totalComment = _context.Comments.Count();
+            string api = "7b5a9c3dd65c9a2fdc219650d657c6c1";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Kocaeli&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument xDocument = XDocument.Load(connection);
+            ViewBag.Temparature = xDocument.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
