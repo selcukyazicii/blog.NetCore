7a4c33ddf362e9a825855a0d21a5b7cb2baae134
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-09-05 11:52:29 +0300

    statistic operations

---
+++ coreProject2/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreProject2.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        //contact manager,comment manager içini doldur,context ile çekme
+        BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+        Context _context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.totalBlog = _blogManager.GetList().Count();
+            ViewBag.totalContact = _context.Contacts.Count();
+            ViewBag.totalComment = _context.Comments.Count();
+            return View();
+        }
+    }
+}
---
+++ coreProject2/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreProject2.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2:ViewComponent
+    {
+        BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.lastBlog= _blogManager.GetList().OrderByDescending(x => x.CreateDate).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}
