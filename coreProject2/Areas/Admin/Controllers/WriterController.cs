1f219f65b340b9185e1d59182a7806e2eeb63441
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-09-06 14:38:10 +0300

    ...

---
+++ coreProject2/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,38 @@
+﻿using coreProject2.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreProject2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id=1,
+                Name="Selo"
+            },
+            new WriterClass
+            {
+                Id=2,
+                Name="Ahmet"
+            }
+        }; 
+    }
+}
