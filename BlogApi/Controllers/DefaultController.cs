bdc33d2caff88ac8cbeee7c83ad1ae13b4a332a8
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-11-01 22:03:42 +0300

    api eklendi

---
+++ BlogApi/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using BlogApi.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEmployeeList()
+        {
+            using (Context context=new Context())
+            {
+                var values = context.Employees.ToList();
+                return Ok(values);
+            }
+        }
+    }
+}
