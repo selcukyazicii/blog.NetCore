894ab0a33bf42c2c621e7b5139b707801b5d24bb
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-12-12 03:01:32 +0300

    ...

---
+++ coreProject2/Controllers/EmployeeTestController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coreProject2.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        public async Task<IActionResult>Index()
+        {
+            var http = new HttpClient();
+            var responseMessage = await http.GetAsync("https://localhost:44303/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        public async Task<IActionResult> AddEmployee(Class1 parameters)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(parameters);
+            StringContent stringContent = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:44303/api/Default", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(parameters);
+        }
+    }
+
+    public class Class1
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
